ac825518b7eba6747380c974fa88bed65bdd3e2a
yehudz <yehuda.zeytim@gmail.com>
2022-10-25 21:03:25 -0600

    build: lint and build script
    Packages are out of sync in the versions

--- apps/placeholder-app/next.config.js
+++ apps/placeholder-app/next.config.js
@@ -4,6 +4,9 @@ const withTM = require('next-transpile-modules')(['ui'])
 const nextConfig = withTM({
   reactStrictMode: true,
   swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true
+  }
 })
 
 module.exports = nextConfig
